ea5ad2d71261506e08103fbb82a0c0eeb511ab05
Zubair Naseer <zubairnaseer88@gmail.com>
2020-06-13 19:47:48 +0500

    chapter 2 done

---
+++ Chapter2/app.js
@@ -0,0 +1,43 @@
+var username;
+
+var myName;
+
+myName = "Zubair Naseer";
+
+var message;
+
+message = "Hello World";
+
+// alert(message);
+
+var name = "Zubair";
+
+var age = 15;
+
+var certificate = "Certified Mobile Application Development";
+
+// alert(name);
+
+// alert(age+" years old");
+
+// alert(certificate);
+
+var slicing = "PIZZA";
+// alert(slicing+"\n"+slicing.slice(0,4)+"\n"+slicing.slice(0,3)+"\n"+slicing.slice(0,2)+"\n"+slicing.slice(0,1));
+
+var email;
+email = "zubairnaseer88@gmail.com";
+
+// alert("My email address is "+email);
+
+var book;
+
+book = "A smarter way to learn JavaScript";
+
+// alert("I am trying to learn "+book);
+
+// document.write("<h1>Yah! I can write HTML content through JavaScript</h1>");
+
+var alrt = "▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬";
+
+alert(alrt);
